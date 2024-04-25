099a911750dd7d13161f661ec111f4db7bf655e8
maxtermax <esnene02@gmail.com>
2024-04-24 23:43:20 -0500

    Adding noUpdate setter

--- src/hooks/useMutations.js
+++ src/hooks/useMutations.js
@@ -1,37 +1,36 @@
 import { useState, useRef } from "react";
 import { useObserver } from "../hooks/useObserver";
 
-const randomId = () => crypto?.randomUUID?.() || Math.random().toString(36).substring(2, 16);
+const randomId = () =>
+  crypto?.randomUUID?.() || Math.random().toString(36).substring(2, 16);
 
-export const useMutations = ({
-  store,
-  id,
-  events,
-  noUpdate = false,
-  onChange,
-}) => {
+export const useMutations = (props = {}) => {
   const [_, rerender] = useState(randomId());
   const value = useRef(null);
 
+  const setNoUpdate = (value) => (props.noUpdate = value);
+
   const handleNotification = (e, resolver) => {
-    for (const event of events) {
+    for (const event of props.events) {
       if (e.value.type === event) {
         const value = e.value?.payload?.value;
-        onChange?.(value, resolver);
-        if (
-          (!e.value.targets || e.value.targets.includes(id)) &&
-          noUpdate === false
-        ) {
-          rerender(randomId());
-        }
+        const hasNotTargets = !e.value?.targets;
+        e?.value?.targets?.forEach?.(
+          (target) =>
+            target === props.id &&
+            props?.onChange?.(value, resolver, setNoUpdate)
+        );
+        if (hasNotTargets) props.onChange?.(value, resolver, setNoUpdate);
+        if (props.noUpdate === true) continue;
+        rerender(randomId());
       }
     }
   };
 
   useObserver({
     listener: handleNotification,
-    contexts: [store.context],
-    observer: store.observer,
+    contexts: [props.store.context],
+    observer: props.store.observer,
   });
 
   return value;
